d318d4dbdca588b6314a9ae513816d26f9d6f988
RaduPetruta <radupetruta19@gmail.com>
2024-04-22 20:37:34 +0300

    aa

--- policedep_frontend/src/App.js
+++ policedep_frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-log1111o" alt="logo" />
         <p>
-          Edit1asasd1 <code>src/App.js</code> and save to reload.
+          Edascit1asasd1 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
